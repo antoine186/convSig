047bb587946ba73a542215df927ac76905cf636a
Antoine Tian <antoine186@hotmail.com>
2019-05-02 17:47:16 +0100

    Moved Data into the correct folder

---
+++ src/testscript.cpp
@@ -0,0 +1,33 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+RcppExport SEXP timesTwo(SEXP x) {
+  double res = as<double>(x) * 2;
+  return wrap(res);
+}
+
+// [[Rcpp::export]]
+RcppExport SEXP timesTwoList(SEXP x) {
+  NumericVector res = as<NumericVector>(x) * 2;
+  return wrap(res);
+}
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+timesTwo(42)
+*/
