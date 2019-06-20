955ca597a6f6f61fd7be892c792204a2ba99468a
Antoine Tian <antoine186@hotmail.com>
2019-06-20 16:32:46 +0100

    Added many more tests for both the ReLU and the exponential transforms

--- tests/testthat/test.ReLU.R
+++ tests/testthat/test.ReLU.R
@@ -1,7 +1,16 @@
 context("Shallow ReLU Tests")
 
-conv3 <- conv_create(32, 5, 3)
-conv5 <- conv_create(512, 5, 5)
+K = 5
+poss3 = (3 * 4) - 2
+feat3 <- matrix(runif(poss3*K), nrow = poss3, ncol = K)
+poss5 = (5 * 4) - 2
+feat5 <- matrix(runif(poss5*K), nrow = poss5, ncol = K)
+complexfeat3 <- array(runif(3 * 4 * K), dim = c(3, 4, K))
+complexfeat5 <- array(runif(5 * 4 * K), dim = c(5, 4, K))
+conv3 <- conv_create(32, 5, 3, feat3)
+conv5 <- conv_create(512, 5, 5, feat5)
+complexconv3 <- complexconv_create(32, 5, 3, complexfeat3, 2)
+complexconv5 <- complexconv_create(512, 5, 5, complexfeat5, 3)
 
 test_that("conv_create returns a well-formed result",
           {
@@ -9,6 +18,15 @@ test_that("conv_create returns a well-formed result",
             expect_equal(dim(conv5)[1], 512)
             expect_equal(dim(conv3)[2], 5)
             expect_equal(dim(conv3)[2], 5)
+          }
+)
+
+test_that("complexconv_create returns a well-formed result",
+          {
+            expect_equal(dim(complexconv3)[1], 32)
+            expect_equal(dim(complexconv5)[1], 512)
+            expect_equal(dim(complexconv3)[2], 5)
+            expect_equal(dim(complexconv3)[2], 5)
           }
 )
 
@@ -41,6 +59,38 @@ test_that("tencode returns a well-formed result",
             expect_equal(dim(ten5)[2], 18)
           }
 )
+
+ar1 <- array(runif(10*5), dim = c(10,1,5))
+ar2 <- array(runif(10*5), dim = c(10,5,1))
+ar3 <- array(runif(10*5), dim = c(1,10,5))
+
+test_that("three_recycle produces the correct format at any designated axis",
+          {
+            expect_equal(dim(three_recycle(ar1, 2, 20)), c(10, 20, 5))
+            expect_equal(dim(three_recycle(ar2, 3, 20)), c(10, 5, 20))   
+            expect_equal(dim(three_recycle(ar3, 1, 20)), c(20, 10, 5))   
+          }
+)
+
+ar1 <- array(runif(10*5*3), dim = c(10,3,5,1))
+ar2 <- array(runif(10*5*3), dim = c(10,5,1,3))
+ar3 <- array(runif(10*5*3), dim = c(3,1,10,5))
+ar4 <- array(runif(10*5*3), dim = c(1,3,10,5))
+
+test_that("four_recycle produces the correct format at any designated axis",
+          {
+            expect_equal(dim(four_recycle(ar1, 4, 20)), c(10,3,5,20))
+            expect_equal(dim(four_recycle(ar2, 3, 20)), c(10,5,20,3))
+            expect_equal(dim(four_recycle(ar3, 2, 20)), c(3,20,10,5))
+            expect_equal(dim(four_recycle(ar4, 1, 20)), c(20,3,10,5))
+          }
+)
+
+# test_that("",
+#           {
+#             expect_equal(, )      
+#           }
+# )
 
 
 
