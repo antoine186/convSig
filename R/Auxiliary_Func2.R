3dc4aeb1167d28c1b168030a16c52b805232f2e4
Antoine Tian <antoine186@hotmail.com>
2019-06-30 12:32:50 +0100

    Added conv computation for ReLU

---
+++ R/Auxiliary_Func2.R
@@ -0,0 +1,43 @@
+relu_conv_all <- function(bg, mat, N, K, type, numbase, feat, mid) {
+  
+  conv <- conv_create(N, K, numbase, feat)
+  conv_all <- array(0, dim = c(N, 3, K))
+  
+  list_len <- length(unlist(type[[mid]][1]))
+  
+  for (i in 1:2) {
+    
+    inter_conv = array(conv[unlist(type[[mid]][i]),], dim = c(list_len, 1, K))
+    
+    inter_bg = array(bg[unlist(type[[mid]][i])], dim = c(list_len, 1, 1))
+    inter_bg <- three_recycle(inter_bg, 3, K)
+    
+    inter_res <- inter_conv * (inter_bg / sum(bg))
+    
+    inter_mat = array(mat[i,,], dim = c(1, 3, K))
+    inter_mat <- three_recycle(inter_mat, 1, list_len) 
+    
+    conv_all[unlist(type[[mid]][i]),,] = three_recycle(inter_res, 2, 3) * inter_mat
+    
+  }
+  
+  #conv_all <- c(t(as.matrix(conv_all)))
+  new_conv_all <- array(0, dim = c(N*3, K))
+  
+  conv_count = 1
+  for (i in 1:N) {
+    for (j in 1:3) {
+      new_conv_all[conv_count, ] = conv_all[i,j,]
+      conv_count = conv_count + 1
+    }
+  }
+  
+  invisible(new_conv_all)
+  
+}
+
+# exp_conv_all <- function(bg, mat, N, K, type, numbase, feat, mid) {
+#   
+#   
+#   
+# }
