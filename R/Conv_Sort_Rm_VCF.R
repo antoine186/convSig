e78a9bdb83948b74871a1143ff9b0faf4b9428d8
Antoine Tian <antoine186@hotmail.com>
2019-12-22 08:56:34 +0000

    Fixed c++ icgc format construction problem

--- R/Conv_Sort_Rm_VCF.R
+++ R/Conv_Sort_Rm_VCF.R
@@ -33,7 +33,11 @@ vcf2mut <- function(datapath, geno = "GT") {
   }
   
   sum_names <- cnames[c((minus_start+1):end_point)]
+  chopped_samples <- unlist(vcf_data[, sum_names, with = FALSE])
   icgc_height <- sum(unlist(vcf_data[, sum_names, with = FALSE]))
+  
+  raw_icgc_form <- icgc_creater(vcf_data[, c(1,2,4,5), with = FALSE], chopped_samples,
+                                sum_names, icgc_height)
   
   ### Under development
   vcf_data <- vcf_data[, c("Chrom", "Pos", "ID", "REF", "ALT"), with=FALSE]
--- src/VcfBackend.cpp
+++ src/VcfBackend.cpp
@@ -5,36 +5,37 @@ using namespace std;
 // This is a simple example of exporting a C++ function to R. You can
 
 // [[Rcpp::export]]
-DataFrame icgc_creater(DataFrame vcf_data, CharacterVector cnames, int height, 
-                    int start_it, int end_it) {
+DataFrame icgc_creater(DataFrame vcf_data, NumericMatrix sample_data,
+                       CharacterVector sample_names, int height) {
   
-  NumericVector num_vec(height);
-  CharacterVector char_vec(height);
-
-  // When giving names to columns
-  DataFrame df = DataFrame::create( Named("icgc_sample_id") = char_vec , 
-                                    Named("chromosome") = num_vec, 
-                                    Named("chromosome_start") = num_vec, 
-                                    Named("chromosome_end") = num_vec, 
-                                    Named("mutated_from_allele") = char_vec, 
-                                    Named("mutated_to_allele") = char_vec);
+  CharacterVector sample_new(height);
+  NumericVector chrom_new(height);
+  NumericVector chrom_start_new(height);
+  CharacterVector ref_new(height);
+  CharacterVector alt_new(height);
+  
+  NumericVector chrom = vcf_data[0];
+  NumericVector pos = vcf_data[1];
+  CharacterVector ref = vcf_data[2];
+  CharacterVector alt = vcf_data[3];
   
   int df_count = 0;
   int cur_val = 0;
+  int end_it = sample_data.ncols();
   
-  for (int i = start_it; i < end_it + 1; i = i + 1) {
+  for (int i = 0; i < end_it; i = i + 1) {
 
     for (int j = 0; j < height; j = j + 1) {
-      
-      cur_val = vcf_data[j, i];
+
+      cur_val = sample_data(j, i);
       if (cur_val == 1) {
 
-        df[df_count, 0] = cnames[i];
-        df[df_count, 1] = vcf_data[j, 0];
-        df[df_count, 2] = vcf_data[j, 1];
-        df[df_count, 4] = vcf_data[j, 3];
-        df[df_count, 5] = vcf_data[j, 4];
-        
+        sample_new[df_count] = sample_names[i];
+        chrom_new[df_count] = chrom[i];
+        chrom_start_new[df_count] = pos[i];
+        ref_new[df_count] = ref[i];
+        alt_new[df_count] = alt[i];
+
         ++df_count;
 
       }
@@ -42,6 +43,13 @@ DataFrame icgc_creater(DataFrame vcf_data, CharacterVector cnames, int height,
     }
 
   }
+  
+  DataFrame df = DataFrame::create( Named("icgc_sample_id") = sample_new,
+                                    Named("chromosome") = chrom_new,
+                                    Named("chromosome_start") = chrom_start_new,
+                                    Named("chromosome_end") = chrom_start_new,
+                                    Named("mutated_from_allele") = ref_new,
+                                    Named("mutated_to_allele") = alt_new);
   
   return df;
   
