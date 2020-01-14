68cc93163379eed99816830ba37b4e4d2146a787
Antoine Tian <antoine186@hotmail.com>
2020-01-14 11:06:14 +0000

    c++ debug attempt

--- src/Chrom_Loops.cpp
+++ src/Chrom_Loops.cpp
@@ -321,6 +321,7 @@ S4 shallow_loop5(S4 mat, DataFrame fasta, DataFrame mut_file, CharacterVector un
         while (1) {
           //++free_stat;
           if(ref_ar[mut_pos] != base3) {
+            std::string err_pos = std::to_string(mut_pos);
             stop("It seems that your supplied assembly input does not " 
                    "correspond to the one in your mutation input file");
             //mut_pos = ++mut_pos;
